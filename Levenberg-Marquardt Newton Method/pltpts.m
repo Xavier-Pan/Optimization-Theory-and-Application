17ff22e5842bc7f5678f29acfceedd3397407b00
Xavier-Pan <sypan.cser@gmail.com>
2018-05-07 12:28:01 +0800

    Delete pltpts.m

--- Levenberg-Marquardt Newton Method/pltpts.m
+++
@@ -1,8 +0,0 @@
-function out=pltpts(xnew,xcurr) 
-    % Plots Two points and connects them via red line   
-    plot([xcurr(1),xnew(1)],[xcurr(2),xnew(2)], 'r - ' ,xnew(1),xnew(2), ... 
-        'bo' ,xcurr(1),xcurr(2), 'ro');
-    drawnow; % Draws current graph now 
-    hold on
-    out = [];
-    
